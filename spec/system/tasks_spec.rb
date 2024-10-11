479462593470d638b0202640dcf6d93befb0779e
takanosatomi <ricebread778899@gmail.com>
2024-10-11 17:59:25 +0900

    テストケース修正

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -12,11 +12,12 @@ RSpec.describe 'タスク管理機能', type: :system do
         # テストで使用するためのタスクを登録
         # Task.create!(title: '書類作成', content: '企画書を作成する。')
         # FactoryBotを使用することで、より簡潔なコードでテストデータを作成する
-        FactoryBot.create(:task)
+        FactoryBot.create(:task, title: '書類作成', content: '企画書を作成する。')
         # タスク一覧画面に遷移
         visit tasks_path
         # visit（遷移）したpage（この場合、タスク一覧画面）に"書類作成"という文字列が、have_content（含まれていること）をexpect（確認・期待）する
         expect(page).to have_content '書類作成'
+        expect(page).to have_content '企画書を作成する。'
         # expectの結果が「真」であれば成功、「偽」であれば失敗としてテスト結果が出力される
       end
     end
@@ -28,13 +29,14 @@ RSpec.describe 'タスク管理機能', type: :system do
         # テストで使用するためのタスクを登録
         # Task.create!(title: '書類作成', content: '企画書を作成する。')
         # FactoryBotを使用することで、より簡潔なコードでテストデータを作成する
-        FactoryBot.create(:task)
+        FactoryBot.create(:task, title: '書類作成', content: '企画書を作成する。')
         # タスク一覧画面に遷移
         visit tasks_path
 
 
         # visit（遷移）したpage（この場合、タスク一覧画面）に"書類作成"という文字列が、have_content（含まれていること）をexpect（確認・期待）する
         expect(page).to have_content '書類作成'
+        expect(page).to have_content '企画書を作成する。'
         # expectの結果が「真」であれば成功、「偽」であれば失敗としてテスト結果が出力される
       end
     end
@@ -48,11 +50,15 @@ RSpec.describe 'タスク管理機能', type: :system do
         # テストで使用するためのタスクを登録
         # Task.create!(title: '書類作成', FactoryBotを使用することで、より簡潔なコードでテストデータを作成するcontent: '企画書を作成する。')
         # 
-        FactoryBot.create(:task)
+        task = FactoryBot.create(:task, title: '書類作成', content: '企画書を作成する。')
+
         # タスク詳細画面に遷移
-        visit tasks_path
+        visit task_path(task)
+
+
         # visit（遷移）したpage（この場合、タスク一覧画面）に"書類作成"という文字列が、have_content（含まれていること）をexpect（確認・期待）する
         expect(page).to have_content '書類作成'
+        expect(page).to have_content '企画書を作成する。'
         # expectの結果が「真」であれば成功、「偽」であれば失敗としてテスト結果が出力される       
        end
      end
