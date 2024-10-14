834733cccdb8dea83588eeab632b7755a49b921d
takanosatomi <ricebread778899@gmail.com>
2024-10-14 17:35:01 +0900

    step2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+# 50件のタスクデータを作成
+50.times do |n|
+  Task.create!(
+    title: "Task_#{n + 1}", # タスクのタイトルをTask_1, Task_2, ... のようにする
+    content: "This is task number #{n + 1}." # タスクの内容をシンプルな文にする
+  )
+end
--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -5,13 +5,13 @@ FactoryBot.define do
   # 作成するテストデータの名前を「task」とします
   # 「task」のように存在するクラス名のスネークケースをテストデータ名とする場合、そのクラスのテストデータが作成されます
   factory :task do
-    title { '書類作成' }
-    content { '企画書を作成する。' }
+    title { "書類作成" }
+    content { "企画書を作成する。" }
   end
   # 作成するテストデータの名前を「second_task」とします
   # 「second_task」のように存在しないクラス名のスネークケースをテストデータ名とする場合、`class`オプションを使ってどのクラスのテストデータを作成するかを明示する必要があります
   factory :second_task, class: Task do
-    title { 'メール送信' }
-    content { '顧客へ営業のメールを送る。' }
+    title { "メール送信" }
+    content { "顧客へ営業のメールを送る。" }
   end
 end
