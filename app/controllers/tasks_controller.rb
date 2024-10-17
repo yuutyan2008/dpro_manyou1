2155845ef32dc90b77dbac0b6e97307c8875cada
takanosatomi <ricebread778899@gmail.com>
2024-10-17 15:30:45 +0900

    first

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,34 @@
 class TasksController < ApplicationController
   def index
+    # 検索パラメータの初期化
+    @search_params = search_params
+    # 検索機能（スコープを適用）
+    @tasks =
+      Task
+        .all
+        .search_by_title(@search_params[:title])
+        .search_by_status(@search_params[:status])
+
+    # puts @search_params.inspect #コンソールでエラー原因の確認に使用
+
+    if params[:sort_deadline_on].present?
+      @tasks = @tasks.sort_by_deadline
+      # 終了期限でソート
+    elsif params[:sort_priority].present?
+      @tasks = @tasks.sort_by_priority
+      # 優先度でソート
+    else
+      @tasks = @tasks.sort_by_created_at
+    end
+
     # ページネーションで1ページあたり10件表示するように設定
-    @tasks = Task.order(created_at: :desc).page(params[:page]).per(10)
+    @tasks = @tasks.page(params[:page]).per(10)
   end
+
+  # # クエリをログに出力（確認用）
+  # puts @tasks.to_sql
+
+  # render :index
 
   def show
     @task = Task.find(params[:id])
@@ -43,6 +69,19 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :content)
+    params.require(:task).permit(
+      :title,
+      :content,
+      :deadline_on,
+      :priority,
+      :status
+    )
+  end
+
+  # ストロングパラメータの設定
+  # このメソッドを追加して、search パラメータ内の title と status だけを許可します。
+  def search_params
+    # search パラメータが存在することを確認し、その中で title と status だけを許可します。
+    params.fetch(:search, {}).permit(:title, :status)
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -8,7 +8,14 @@ RSpec.describe "タスクモデル機能", type: :model do
       # itには「期待する動作」について記載
       it "バリデーションに失敗する" do
         # テストケース
-        task = Task.create(title: "", content: "企画書を作成する。")
+        task =
+          Task.create(
+            title: "",
+            content: "企画書を作成する。",
+            deadline_on: Date.today,
+            priority: 1,
+            status: 1
+          )
         expect(task).not_to be_valid
       end
     end
@@ -16,17 +23,125 @@ RSpec.describe "タスクモデル機能", type: :model do
     context "タスクの説明が空文字の場合" do
       it "バリデーションに失敗する" do
         # テストケース
-        task = Task.create(title: "企画書", content: "")
+        task =
+          Task.create(
+            title: "企画書",
+            content: "",
+            deadline_on: Date.today,
+            priority: 1,
+            status: 1
+          )
+        expect(task).not_to be_valid
+      end
+    end
+    # タスクの期限が空の場合
+    context "タスクの期限が空の場合" do
+      it "バリデーションに失敗する" do
+        task =
+          Task.create(
+            title: "企画書",
+            content: "企画書を作成する。",
+            deadline_on: nil,
+            priority: 1,
+            status: 1
+          )
         expect(task).not_to be_valid
       end
     end
 
-    context "タスクのタイトルと説明に値が入っている場合" do
+    # タスクの優先度が空の場合
+    context "タスクの優先度が空の場合" do
+      it "バリデーションに失敗する" do
+        task =
+          Task.create(
+            title: "企画書",
+            content: "企画書を作成する。",
+            deadline_on: Date.today,
+            priority: nil,
+            status: 1
+          )
+        expect(task).not_to be_valid
+      end
+    end
+
+    # タスクのステータスが空の場合
+    context "タスクのステータスが空の場合" do
+      it "バリデーションに失敗する" do
+        task =
+          Task.create(
+            title: "企画書",
+            content: "企画書を作成する。",
+            deadline_on: Date.today,
+            priority: 1,
+            status: nil
+          )
+        expect(task).not_to be_valid
+      end
+    end
+
+    # タスクのタイトル、説明、期限、優先度、ステータスが全て正しい場合
+    context "全ての属性が正しい場合" do
       it "タスクを登録できる" do
-        # テストケース
-        task = Task.create(title: "企画書", content: "企画書を作成する。")
-        # 条件が成功することを
+        task =
+          Task.create(
+            title: "企画書",
+            content: "企画書を作成する。",
+            deadline_on: Date.today,
+            priority: 1,
+            status: 1
+          )
         expect(task).to be_valid
+      end
+    end
+  end
+
+  # 万葉課題3
+  describe "検索機能" do
+    # テストデータを作成
+    let!(:first_task) do
+      FactoryBot.create(:task, title: "first_task_title", status: :not_started)
+    end
+    let!(:second_task) do
+      FactoryBot.create(:task, title: "second_task_title", status: :under_way)
+    end
+    let!(:third_task) do
+      FactoryBot.create(:task, title: "third_task_title", status: :completed)
+    end
+
+    context "scopeメソッドでタイトルのあいまい検索をした場合" do
+      it "検索ワードを含むタスクが絞り込まれる" do
+        expect(Task.search_by_title("first")).to include(first_task)
+        expect(Task.search_by_title("first")).not_to include(second_task)
+        expect(Task.search_by_title("first")).not_to include(third_task)
+        expect(Task.search_by_title("first").count).to eq 1
+      end
+    end
+
+    context "scopeメソッドでステータス検索をした場合" do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        expect(Task.search_by_status("not_started")).to include(first_task)
+        expect(Task.search_by_status("not_started")).not_to include(second_task)
+        expect(Task.search_by_status("not_started")).not_to include(third_task)
+        expect(Task.search_by_status("not_started").count).to eq 1
+      end
+    end
+
+    context "scopeメソッドでタイトルのあいまい検索とステータス検索をした場合" do
+      it "検索ワードをタイトルに含み、かつステータスに完全一致するタスクが絞り込まれる" do
+        binding.irb
+
+        expect(
+          Task.search_by_title("first").search_by_status("not_started")
+        ).to include(first_task)
+        expect(
+          Task.search_by_title("first").search_by_status("not_started")
+        ).not_to include(second_task)
+        expect(
+          Task.search_by_title("first").search_by_status("not_started")
+        ).not_to include(third_task)
+        expect(
+          Task.search_by_title("first").search_by_status("not_started").count
+        ).to eq 1
       end
     end
   end
